cec2aa9a7d21069d12a6da951c39010c5f6a51da
chans <imagechans@gmail.com>
2024-08-16 15:06:29 +0800

    update code

--- votings/serializers.py
+++ votings/serializers.py
@@ -27,7 +27,7 @@ class GetNodeMetaDataSerializer(serializers.Serializer):
 
 class NodeToUserVoteTotalsSerializer(serializers.Serializer):
     keypair = serializers.CharField(required=True)
-    node_id = serializers.CharField(required=False)
+    node_id = serializers.CharField(required=True)
     user_id = serializers.CharField(required=False)
 
 
