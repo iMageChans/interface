9ca06c4a3c2821d5cdab11a7f3867e742621e68c
chans <imagechans@gmail.com>
2024-08-29 17:21:42 +0800

    update code

--- amm/abi/files.py
+++ amm/abi/files.py
@@ -2,4 +2,4 @@ import os
 
 
 def get_abi_files():
-    return os.path.join(os.path.dirname(__file__), 'market_maker.json')
+    return os.path.join(os.path.dirname(__file__), 'test_market_maker.json')
