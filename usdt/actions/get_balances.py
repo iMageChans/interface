287359bd448a2a6bc9cd1b347002f92b11332b76
chans <imagechans@gmail.com>
2024-08-29 16:16:43 +0800

    update code

--- usdt/actions/get_balances.py
+++ usdt/actions/get_balances.py
@@ -1,22 +1,21 @@
 from base.actions import BaseActionsRead
+from usdt.service.read import Read
 from usdt.tasks import update_or_create_usdt_balance_celery
 from users_profile.serializers import *
+from utils import numbers
+from utils.JSONExtractor import extractor
 
 
 class GetBalances(BaseActionsRead):
     def __init__(self, validated_data):
         super().__init__(validated_data)
-        try:
-            usdt_balances = USDTBalances.objects.get(pk=self.account_id.mate_data_address())
-            self.results = USDTBalancesSerializer(usdt_balances).data
-        except USDTBalances.DoesNotExist:
-            self.results = update_or_create_usdt_balance_celery(
-                account_id=self.account_id.mate_data_address(),
-                keypair=self.keypair.private_key.hex()
-            )
+        usdt_read = Read(self.keypair)
+        self.results = usdt_read.balance_of(owner=self.account_id.get_valid_address())
 
     def serializers(self):
-        return self.results
+        return {"balance_usdt": numbers
+                    .DecimalTruncation(2)
+                    .format_usdt(extractor.get_data_or_err(self.results.value_serialized))}
 
     def is_success(self):
         return True
