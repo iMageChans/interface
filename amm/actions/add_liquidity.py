0e1fa95448666d4df71c05cdb33ffa5e6ed11c82
chans <imagechans@gmail.com>
2024-08-29 15:46:51 +0800

    update code

--- amm/actions/add_liquidity.py
+++ amm/actions/add_liquidity.py
@@ -20,22 +20,17 @@ class AddLiquidity(BaseActionsExec):
             usdt_amount=self.usdt_amount,
             d9_amount=self.d9_amount
         )
-        d9_balance = update_or_create_d9_balance_celery.delay(self.account_id.mate_data_address())
-        usdt_balance = update_or_create_usdt_balance_celery.delay(self.account_id.mate_data_address(),
-                                                                  self.keypair.private_key.hex())
-        update_or_created_token_market_information_celery.delay()
+
 
 
     def serializers(self):
         values = extractor.get_transfer_data(self.results)
-        values.update({"block_number": self.exec.get_block_numbers(self.results.block_hash)})
-        values.update({"form_address": self.account_id.mate_data_address()})
-        values.update({"to_address": "Dn" + self.results.contract_address})
-        values.update({"d9": numbers.format_d9(self.d9_amount)})
-        values.update({"usdt": numbers.format_d9(self.usdt_amount)})
-        values.update({"actions": "add_liquidity"})
-        models.Transaction.objects.create(**values)
         return values
 
     def is_success(self):
+        if self.results.is_success:
+            d9_balance = update_or_create_d9_balance_celery.delay(self.account_id.mate_data_address())
+            usdt_balance = update_or_create_usdt_balance_celery.delay(self.account_id.mate_data_address(),
+                                                                      self.keypair.private_key.hex())
+            update_or_created_token_market_information_celery.delay()
         return self.results.is_success
--- amm/actions/get_usdt.py
+++ amm/actions/get_usdt.py
@@ -18,22 +18,15 @@ class GetUSDT(BaseActionsExec):
             d9_amount=numbers.to_d9(self.d9),
         )
 
-        d9_balance = update_or_create_d9_balance_celery.delay(self.account_id.mate_data_address())
-        usdt_balance = update_or_create_usdt_balance_celery.delay(self.account_id.mate_data_address(),
-                                                                  self.keypair.private_key.hex())
-        update_or_created_token_market_information_celery.delay()
-
 
     def serializers(self):
-        read = Read(self.keypair)
-        data = extractor.get_data_or_err(read.get_usdt(d9_amount=numbers.to_d9(self.d9)).value_serialized)
-        usdt = numbers.format_usdt(data)
         values = extractor.get_transfer_data(self.results)
-        values.update({"block_number": read.get_block_numbers(self.results.block_hash)})
-        values.update({"form_address": self.account_id.mate_data_address()})
-        values.update({"to_address": "Dn" + self.results.contract_address})
-        values.update({"d9": self.d9})
-        values.update({"usdt": usdt})
-        values.update({"actions": "get_usdt"})
-        models.Transaction.objects.create(**values)
         return values
+
+    def is_success(self):
+        if self.results.is_success:
+            d9_balance = update_or_create_d9_balance_celery.delay(self.account_id.mate_data_address())
+            usdt_balance = update_or_create_usdt_balance_celery.delay(self.account_id.mate_data_address(),
+                                                                      self.keypair.private_key.hex())
+            update_or_created_token_market_information_celery.delay()
+        return self.results.is_success
