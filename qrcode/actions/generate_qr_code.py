998b1c56887eb5f19882ebc65a8301566ca704fd
chans <imagechans@gmail.com>
2024-09-04 16:52:12 +0800

    update code

--- qrcode/actions/generate_qr_code.py
+++ qrcode/actions/generate_qr_code.py
@@ -7,12 +7,14 @@ class GenerateQRCode:
     def __init__(self, request: HttpRequest, validated_data):
         keypair = keystone.check_keypair(validated_data['keypair'])
         amount = validated_data['amount']
+        type = validated_data['type']
         base_url = request.build_absolute_uri('/api/qrcode/process/')
         if base_url.startswith('http://'):
             base_url = base_url.replace('http://', 'https://', 1)
         data = {
             "account_id": f"Dn{keypair.ss58_address}",
             "amount": amount,
+            "type": type,
         }
         query_params = urlencode(data)
         self.qr_code_link = f"{base_url}?{query_params}"
--- qrcode/serializers.py
+++ qrcode/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 class GenerateQRCodeSerializer(serializers.Serializer):
     keypair = serializers.CharField(required=True)
     amount = serializers.IntegerField(required=True)
+    type = serializers.CharField(required=True)
 
 
 class ProcessQRCodeSerializer(serializers.Serializer):
