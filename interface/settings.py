aa9c2d8fb0ffc8f2e9b0b32a5e0507e049acf753
chans <imagechans@gmail.com>
2024-09-09 16:41:08 +0800

    update code

--- interface/settings.py
+++ interface/settings.py
@@ -27,13 +27,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# CSRF_TRUSTED_ORIGINS = [
-#     'http://localhost:5173',
-#     'https://d9-test-server.q6z4kzhr.uk',
-# ]
-#
-# CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'https://d9-test-server.q6z4kzhr.uk',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:5173',
+    'https://d9-test-server.q6z4kzhr.uk',
+]
+
+
+# CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
