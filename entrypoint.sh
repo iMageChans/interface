54b05fde09a9a32c56ce89d134a56ff93e146644
chans <imagechans@gmail.com>
2024-09-10 14:25:29 +0800

    update code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # 等待数据库服务启动
-while ! nc -z db 5432; do
-  echo "Waiting for PostgreSQL..."
-  sleep 1
-done
+#while ! nc -z db 5432; do
+#  echo "Waiting for PostgreSQL..."
+#  sleep 1
+#done
 
 # 运行数据库迁移
 python manage.py migrate
