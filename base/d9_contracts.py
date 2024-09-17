69e129e0f08e75f4ff981529a20548aa78f30c24
chans <imagechans@gmail.com>
2024-09-17 14:41:18 +0800

    update code

--- base/d9_contracts.py
+++ base/d9_contracts.py
@@ -1,46 +1,15 @@
-from urllib.parse import urlparse
-
 from scalecodec.types import GenericCall
-import threading
 from base.config import PYTHON_MAIN_NET_URL
 from substrateinterface.contracts import ContractInstance
 from substrateinterface import Keypair, SubstrateInterface
 from dogpile.cache import make_region
 from typing import Optional
-import ssl
 
-from interface import settings
 
-redis_cache = settings.CACHES.get('default')
-
-if redis_cache and 'redis' in redis_cache['BACKEND'].lower():
-    redis_location = redis_cache['LOCATION']
-
-    # 解析 Redis 的连接 URL
-    parsed_redis_url = urlparse(redis_location)
-
-    redis_host = parsed_redis_url.hostname or 'localhost'
-    redis_port = parsed_redis_url.port or 6379
-    redis_db = int(parsed_redis_url.path.lstrip('/')) if parsed_redis_url.path else 0
-    redis_password = parsed_redis_url.password or None
-
-    # 配置 dogpile.cache 的缓存区域，使用 Redis 作为后端，不包含 SSL 配置
-    region = make_region().configure(
-        'dogpile.cache.redis',
-        expiration_time=3600,
-        arguments={
-            'host': redis_host,
-            'port': redis_port,
-            'db': redis_db,
-            'password': redis_password,
-        }
-    )
-else:
-    # 如果没有配置 Redis，使用内存缓存作为默认选项
-    region = make_region().configure(
-        'dogpile.cache.memory',
-        expiration_time=3600,
-    )
+region = make_region().configure(
+    'dogpile.cache.memory',
+    expiration_time=3600,
+)
 
 ws_options = {
     'ping_interval': 30,
